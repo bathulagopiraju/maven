1d6d319494e4d3883bd932aff6149359469f5aa0
bathulagopiraju <130970876+bathulagopiraju@users.noreply.github.com>
2023-05-02 12:08:52 +0530

    Create cicd.groovy

---
+++ cicd.groovy
@@ -0,0 +1,17 @@
+def newGit(repo)
+{
+   git "http://github.com/bathulagopiraju/${repo}.git"
+}
+
+def newMaven()
+{
+   sh 'mvn package'
+}
+
+def newDeploy(jobname,ipaddress,context)
+{
+   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat9/webapps/${context}.war"
+}
+
+def executedSelenium(jobname)
+{
